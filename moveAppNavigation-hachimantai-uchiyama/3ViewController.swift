3c84731a03b3f880abba1796a9cd97c342364623
uchiyama <ohfortomorrow@gmail.com>
2016-11-04 18:12:23 +0900

    first commit

---
+++ moveAppNavigation-hachimantai-uchiyama/3ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  3ViewController.swift
+//  moveAppNavigation-hachimantai-uchiyama
+//
+//  Created by UCHIYAMA M on 2016/11/04.
+//  Copyright © 2016年 UCHIYAMA M. All rights reserved.
+//
+
+import UIKit
+
+class _ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
